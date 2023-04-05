c7fa8b9fca4a716637ca89ebcc007c52861be8f3
Abhijeet Anand <46624621+anandabhijeet@users.noreply.github.com>
2023-04-05 10:04:56 +0530

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="font-inter">Hell0</h1>
+      <p className="font-montserrat">testing fonts</p>
+    </main>
+    
+  )
+}
